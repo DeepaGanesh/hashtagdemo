a6b98a1eb9354e6844dd28374f9b25c5ac4933c3
Deepa Ganesh <deepsganesh20@gmail.com>
2014-12-18 08:04:21 +0530

    initial commit

---
+++ gen/com/drajk/instaashare/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.drajk.instaashare;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/drajk/instaashare/MainActivity.java
@@ -0,0 +1,70 @@
+package com.drajk.instaashare;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+	private static final String AUTHURL = "https://api.instagram.com/oauth/authorize/";
+	// Used for Authentication.
+	private static final String TOKENURL = "https://api.instagram.com/oauth/access_token";
+	// Used for getting token and User details.
+	public static final String APIURL = "https://api.instagram.com/v1";
+	// Used to specify the API version which we are going to use.
+//	public static String CALLBACKURL = "https://www.google.com";
+	public static String CALLBACKURL = "https://www.google.com";
+	// The callback url that we have used while registering the application.
+
+	public String authURLString, tokenURLString, client_id, client_secret;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		client_id = getResources().getString(R.string.client_id);
+		client_secret = getResources().getString(R.string.client_secret);
+		authURLString = AUTHURL
+				+ "?client_id="
+				+ client_id
+				+ "&redirect_uri="
+				+ CALLBACKURL
+				+ "&response_type=code&display=touch&scope=likes+comments+relationships";
+		tokenURLString = TOKENURL + "?client_id=" + client_id
+				+ "&client_secret=" + client_secret + "&redirect_uri="
+				+ CALLBACKURL + "&grant_type=authorization_code";
+		InstagramApp mApp = new InstagramApp(this, 
+				client_id, 
+				client_secret, 
+				CALLBACKURL);
+		mApp.authorize();
+	    
+		/*WebView webView = new WebView(getApplicationContext());
+		webView.setVerticalScrollBarEnabled(false);
+		webView.setHorizontalScrollBarEnabled(false);
+		webView.setWebViewClient(new AuthWebViewClient());
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.loadUrl(authURLString);*/
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
